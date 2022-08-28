428545f735be5c41f0bc667e9b53aff6b5b40557
srinivasGokarla <srinivasgokarla@gmail.com>
2022-08-28 18:10:04 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,40 @@
-import express from "express";
-import mongoose from "mongoose";
-
-import key from "./keys.js";
-import authRouter from "./router/auth";
-
+const express = require("express");
 const app = express();
-const PORT = 3000 | process.env;
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const profileRoutes = require("./routes/profileRoutes");
 
-mongoose
-  .connect(key.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected");
-  })
-  .catch((err) => console.log("Error", err));
 
 app.use(express.json());
-app.use("/", authRouter);
+app.use(cors());
+app.use(cookieParser());
 
-app.listen(PORT, () => {
-  console.log(`Server started on ${3000}`);
+
+const mongoUrl = process.env.MONGODB_URL;
+mongoose.connect(mongoUrl, err => {
+  if (err) throw err;
+  console.log("Mongodb connected...");
+});
+
+
+
+app.get("/", (req, res) => res.json({ msg: "Hello" }));
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/profile", profileRoutes);
+
+
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.resolve(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html")));
+}
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Backend is running on port ${port}`);
 });
